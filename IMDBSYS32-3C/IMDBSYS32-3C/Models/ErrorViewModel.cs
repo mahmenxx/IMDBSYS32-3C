8d6f32daad36fe182aad3ad5264218a623d93296
apoxxllo <jilabrador019@gmail.com>
2026-01-29 18:05:07 +0800

    initial commit

---
+++ IMDBSYS32-3C/IMDBSYS32-3C/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IMDBSYS32_3C.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
